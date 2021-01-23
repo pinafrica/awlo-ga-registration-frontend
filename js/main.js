4ac4751a927171378a37d7dbfd46198088914e83
Benson Imoh,ST <benson@stbensonimoh.com>
2021-01-23 20:53:22 +0100

    fix: add croppie, change the modal type and move the main script to the bottom

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 const profileImageUpload = () => {
-    const uploadButton = document.getElementById('camera-icon')
-    const uploadFile = document.getElementById('file-upload')
+  const uploadButton = document.getElementById('camera-icon')
+  const uploadFile = document.getElementById('file-upload')
 
-    uploadButton
-      .addEventListener('click', (e) => {
-        uploadFile.click()
-    })
+  uploadButton.addEventListener('click', e => {
+    uploadFile.click()
+  })
 
-    uploadFile.addEventListener('change', (e) =>{
-        readImageFile(e.target.files)
-    })
-    
+  uploadFile.addEventListener('change', e => {
+    readImageFile(e.target.files)
+  })
 }
 
-const readImageFile = (upload) => {
-const profilePicture = document.getElementById('profile-image')
+var modal = document.querySelector('.modal')
+var el = document.querySelector('.croppies')
+var vanilla = new Croppie(el, {
+  viewport: { width: 300, height: 300, type: 'circle' },
+  boundary: { width: 400, height: 400 },
+  customClass: 'croppies'
+})
 
-    if (upload && upload[0]){
-        const reader = new FileReader()
-        reader.addEventListener('load', (e) => {
-          profilePicture.src = e.target.result
-        })
-        reader.readAsDataURL(upload[0])
-    }
+const readImageFile = upload => {
+  const profilePicture = document.getElementById('profile-image')
+
+  if (upload && upload[0]) {
+    const reader = new FileReader()
+    reader.addEventListener('load', e => {
+      modal.classList.add('is-active')
+
+      vanilla.bind({
+        url: e.target.result
+      })
+    })
+    reader.readAsDataURL(upload[0])
+    profilePicture.src = e.target.result
+  }
+
+  //   vanilla.result({ type: 'canvas', size: 'viewport' }).then()
 }
