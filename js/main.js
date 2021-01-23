53fdc073c4dfe97ffbcbc1fb30ad5844d3c38c5b
Chiamaka Osumgba <osumgbachiamaka@gmail.com>
2021-01-23 07:47:22 +0100

    feat: Implement image upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 const profileImageUpload = () => {
-    document.getElementById('camera-icon')
-      .addEventListener('click', function (){
-        document.getElementById('file-upload').click()
+    const uploadButton = document.getElementById('camera-icon')
+    const uploadFile = document.getElementById('file-upload')
+
+    uploadButton
+      .addEventListener('click', (e) => {
+        uploadFile.click()
     })
+
+    uploadFile.addEventListener('change', (e) =>{
+        readImageFile(e.target.files)
+    })
+    
+}
+
+const readImageFile = (upload) => {
+const profilePicture = document.getElementById('profile-image')
+
+    if (upload && upload[0]){
+        const reader = new FileReader()
+        reader.addEventListener('load', (e) => {
+          profilePicture.src = e.target.result
+        })
+        reader.readAsDataURL(upload[0])
+    }
 }
