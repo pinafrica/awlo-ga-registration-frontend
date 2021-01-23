500205f16f19e170dabf11d49045b97a7570e16c
Chiamaka Osumgba <osumgbachiamaka@gmail.com>
2021-01-23 23:16:54 +0100

    fix: display cropped image

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+window.onload = e => profileImageUpload()
+const uploadFile = document.getElementById('file-upload')
+const preview = document.querySelector('.croppies')
+let imageModal = document.querySelector('.modal')
+let vanilla = new Croppie(preview, {
+  viewport: { width: 200, height: 200, type: 'circle' },
+  boundary: { width: 300, height: 300 },
+  customClass: 'croppies'
+})
+
 const profileImageUpload = () => {
   const uploadButton = document.getElementById('camera-icon')
-  const uploadFile = document.getElementById('file-upload')
-
   uploadButton.addEventListener('click', e => {
     uploadFile.click()
   })
@@ -11,29 +19,34 @@ const profileImageUpload = () => {
   })
 }
 
-var modal = document.querySelector('.modal')
-var el = document.querySelector('.croppies')
-var vanilla = new Croppie(el, {
-  viewport: { width: 300, height: 300, type: 'circle' },
-  boundary: { width: 400, height: 400 },
-  customClass: 'croppies'
-})
 
-const readImageFile = upload => {
-  const profilePicture = document.getElementById('profile-image')
+const readImageFile = (upload) => {
 
   if (upload && upload[0]) {
     const reader = new FileReader()
     reader.addEventListener('load', e => {
-      modal.classList.add('is-active')
+        imageModal.classList.add('is-active')
 
-      vanilla.bind({
-        url: e.target.result
-      })
+        vanilla.bind({
+            url: e.target.result
+        })
+
+        bindProfilePicture(vanilla, imageModal) 
     })
     reader.readAsDataURL(upload[0])
-    profilePicture.src = e.target.result
   }
+}
 
-  //   vanilla.result({ type: 'canvas', size: 'viewport' }).then()
+const bindProfilePicture = (vanilla, imageModal) => {
+    const profilePicture = document.getElementById('profile-image')
+    const profileOkBtn = document.querySelector('.cropBtn')
+
+    profileOkBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        vanilla.result('blob').then((blob) => {
+            const url = URL.createObjectURL(blob)
+            return profilePicture.src = url
+        })
+        imageModal.classList.remove('is-active')
+    })
 }
