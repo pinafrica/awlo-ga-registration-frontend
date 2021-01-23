ff1f0d2a125ef3d3a19ec87e19f12abf349f6371
Chiamaka Osumgba <osumgbachiamaka@gmail.com>
2021-01-23 06:51:24 +0100

    style: add camera icon implement picture upload when camera icon is clicked

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const profileImageUpload = () => {
+    document.getElementById('camera-icon')
+      .addEventListener('click', function (){
+        document.getElementById('file-upload').click()
+    })
+}
